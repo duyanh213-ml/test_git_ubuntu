0aff575358618e0239a7b97950d4f3c0fd07ff38
AnhND <anhnd@pmr.vn>
2023-07-03 11:22:10 +0700

    Add calculation in main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 print("Just for test vim")
 print("This is just a second line")
+print("Let's do some math with me:")
+first_num = int(input("The first number is: "))
+second_num = int(input("The second number is: "))
+
+print(f"The total sum is {first_num + second_num}")
